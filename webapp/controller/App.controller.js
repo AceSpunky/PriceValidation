29e0a0440009449936ca0227a2396d84cfc4d209
AceSpunky <49279281+AceSpunky@users.noreply.github.com>
2026-05-13 08:11:32 +0000

    First Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("com.lipton.pricevalidation.controller.App", {
+      onInit() {
+      }
+  });
+});
